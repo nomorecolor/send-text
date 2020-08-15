2b09f9e160f74a6aaf50ac44f610a6e380e3e647
Alain Elemia <ElemiaAT@etpi.com.ph>
2020-08-15 18:09:08 +0800

    Remove example folder

--- example/src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import SendText from '@nomorecolor/send-text'
-
-const App = () => {
-  return <SendText countryCode='63' onSubmit={() => {}} />
-}
-
-export default App
