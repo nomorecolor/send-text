d0ae5904cfe32e9eeb630c54d3ebb95af01b8d76
Alain Elemia <ElemiaAT@etpi.com.ph>
2020-08-15 18:14:08 +0800

    Updates

---
+++ precache-manifest.025133d1e70d88b40200e9ead80b9669.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96c9b8d40e03811f8a459812d9db1208",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7ad18de515fd3f72d857",
+    "url": "./static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "bebda92009d4da8328af",
+    "url": "./static/js/2.5fd8027d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.5fd8027d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ad18de515fd3f72d857",
+    "url": "./static/js/main.4ed5c90d.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.4ed5c90d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0214e5c08377cbb69238",
+    "url": "./static/js/runtime-main.8d5171ce.js"
+  }
+]);
